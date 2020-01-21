75a0349aa405caa625b702ce18c1556e29ead98e
sasaju <1289142675@qq.com>
2020-01-21 19:19:46 +0800

    master

--- scrape_wiki.py
+++ scrape_wiki.py
@@ -1,8 +1,22 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
+import datetime
+import random
 
-html = urlopen("https://en.wikipedia.org/wiki/Kevin_Bacon")
-bs0bj = BeautifulSoup(html)
+random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+    html = urlopen("https://en.wikipedia.org"+articleUrl)
+    bs0bj = BeautifulSoup(html)
+    return bs0bj.find("div", {"id":"bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
+
+links = getLinks("/wiki/Kevin_Bacon")
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+    print(newArticle)
+    links = getLinks(newArticle)
+"""
 for link in bs0bj.findAll("a"):
     if 'href' in link.attrs:
         print(link.attrs['href'])
+"""
